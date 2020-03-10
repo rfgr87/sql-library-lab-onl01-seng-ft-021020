bf754edd609e946fcc87b865e0c926338441868c
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-10 23:25:17 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,8 +14,8 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name
   FROM series 
-  INNER JOIN authors ON authors.id = series.author_id
-  INNER JOIN subgenres ON series.subgenre_id = subgenres.id"
+  INNER JOIN authors ON authors.id = author_id
+  INNER JOIN subgenres ON subgenre_id = subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
